438f962a2ed0d4d19c0a050852c3f401fe10df90
Tim Chen <chengt@gmail.com>
2011-07-21 03:15:37 -0500

    updated m2 completions

--- m2-completion.bash
+++ m2-completion.bash
@@ -1,32 +1,28 @@
-# Bash Maven2 completion
-#
-_mvn()
-{
-  local cmds cur colonprefixes
-  cmds="clean validate compile test package integration-test \
-  verify install deploy test-compile site generate-sources \
-  process-sources generate-resources process-resources \
-  idea:idea -DartifactId= -DgroupId= -DskipTests \
-  -DarchetypeArtifactId= -Dverbose \
-  jetty:run exec:java exec:exec \
-  migration:new migration:up migration:down -Dmigration.description= \
-  dependency:analyze dependency:resolve dependency:tree \
-  versions:display-dependency-updates versions:display-plugin-updates \
-  javadoc:aggregate javadoc:aggregate-jar \
-  source:aggregate"
-  COMPREPLY=()
-  cur=${COMP_WORDS[COMP_CWORD]}
-  # Work-around bash_completion issue where bash interprets a colon
-  # as a separator.
-  # Work-around borrowed from the darcs work-around for the same
-  # issue.
-  colonprefixes=${cur%"${cur##*:}"}
-  COMPREPLY=( $(compgen -W '$cmds' -- $cur))
-  local i=${#COMPREPLY[*]}
-  while [ $((--i)) -ge 0 ]; do
-    COMPREPLY[$i]=${COMPREPLY[$i]#"$colonprefixes"}
-  done
-  return 0
-} &&
+#!/bin/bash
 
-complete -F _mvn mvn
+_m2_make_goals()
+{
+plugin=$1
+mojos=$2
+for mojo in $mojos
+do
+export goals="$goals $plugin:$mojo"
+done
+}
+
+_m2_complete()
+{
+local cur goals
+
+COMPREPLY=()
+cur=${COMP_WORDS[COMP_CWORD]}
+goals='clean compile test install package deploy site'
+goals=$goals _m2_make_goals "eclipse" "eclipse"
+goals=$goals _m2_make_goals "idea" "idea"
+goals=$goals _m2_make_goals "assembly" "assembly"
+goals=$goals _m2_make_goals "plexus" "app bundle-application bundle-runtime descriptor runtime service"
+cur=`echo $cur | sed 's/\\\\//g'`
+COMPREPLY=($(compgen -W "${goals}" ${cur} | sed 's/\\\\//g') )
+}
+
+complete -v -F _m2_complete -f -o filenames -o dirnames mvn
