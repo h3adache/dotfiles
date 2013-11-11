dbed51f9f996b94734de10ad6bf272e440e50617
Tim Chen <chengt@gmail.com>
2013-11-11 15:46:27 -0600

    add xmonad, favor xmonad over wmii

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,26 @@
+import XMonad
+import XMonad.Config.Gnome
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.ManageDocks
+
+import Text.Regex.Posix ((=~))
+
+q ~? x = fmap (=~ x) q
+myManageHook = resource =? "sun-awt-X11-XWindowPeer" <&&> title ~? "win[0-9]+" --> doIgnore
+defaultModMask = mod4Mask
+main = do
+   xmonad $ gnomeConfig {
+     manageHook = myManageHook <+> manageHook gnomeConfig
+     , startupHook = setWMName "LG3D"
+     , logHook = ewmhDesktopsLogHook
+     , modMask = defaultModMask
+     , terminal = "urxvt"
+     }`additionalKeys`
+     [ ((mod4Mask, xK_p), spawn "dmenu_run -b")
+        -- resizing the master/slave ratio
+        , ((mod4Mask,               xK_h     ), sendMessage Shrink) -- %! Shrink the master area
+        , ((mod4Mask .|. shiftMask, xK_h     ), sendMessage Expand) -- %! Expand the master area
+     ]
