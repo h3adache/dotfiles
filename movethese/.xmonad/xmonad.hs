3f77a686bb698ebc55cc3922b13b3d7d76053b09
Tim Chen <chengt@gmail.com>
2015-12-28 13:10:01 -0600

    updated dotfiles

--- movethese/.xmonad/xmonad.hs
+++ movethese/.xmonad/xmonad.hs
@@ -1,26 +1,76 @@
 import XMonad
-import XMonad.Config.Gnome
-import XMonad.Hooks.SetWMName
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.Grid
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.IM
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Circle
+import XMonad.Layout.PerWorkspace (onWorkspace)
+import XMonad.Layout.Fullscreen
+import System.IO
 
-import Text.Regex.Posix ((=~))
-
-q ~? x = fmap (=~ x) q
-myManageHook = resource =? "sun-awt-X11-XWindowPeer" <&&> title ~? "win[0-9]+" --> doIgnore
-defaultModMask = mod4Mask
+modm = mod4Mask     -- Rebind Mod to the Windows key
 main = do
-   xmonad $ gnomeConfig {
-     manageHook = myManageHook <+> manageHook gnomeConfig
-     , startupHook = setWMName "LG3D"
-     , logHook = ewmhDesktopsLogHook
-     , modMask = defaultModMask
-     , terminal = "urxvt"
-     }`additionalKeys`
-     [ ((defaultModMask, xK_p), spawn "dmenu_run -b")
-        -- resizing the master/slave ratio
-        , ((defaultModMask,               xK_h     ), sendMessage Shrink) -- %! Shrink the master area
-        , ((defaultModMask .|. shiftMask, xK_h     ), sendMessage Expand) -- %! Expand the master area
-     ]
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/tchen/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = defaultLayouts
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        } >>
+                    ewmhDesktopsStartup >> setWMName "LG3D"
+        , modMask = modm
+        , terminal = "urxvt"
+        , startupHook = ewmhDesktopsStartup >> setWMName "LG3D"
+        } `additionalKeys`
+        [ ((modm .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        , ((modm,               xK_a), sendMessage MirrorShrink)
+        , ((modm,               xK_z), sendMessage MirrorExpand)
+        ]
+
+-- Define group of default layouts used on most screens, in the
+-- order they will appear.
+-- "smartBorders" modifier makes it so the borders on windows only
+-- appear if there is more than one visible window.
+-- "avoidStruts" modifier makes it so that the layout provides
+-- space for the status bar at the top of the screen.
+defaultLayouts = smartBorders(avoidStruts(
+  -- ResizableTall layout has a large master window on the left,
+  -- and remaining windows tile on the right. By default each area
+  -- takes up half the screen, but you can resize using "super-h" and
+  -- "super-l".
+  ResizableTall 1 (3/100) (1/2) []
+
+  -- Mirrored variation of ResizableTall. In this layout, the large
+  -- master window is at the top, and remaining windows tile at the
+  -- bottom of the screen. Can be resized as described above.
+  ||| Mirror (ResizableTall 1 (3/100) (1/2) [])
+
+  -- Full layout makes every window full screen. When you toggle the
+  -- active window, it will bring the active window to the front.
+  ||| noBorders Full
+
+  -- ThreeColMid layout puts the large master window in the center
+  -- of the screen. As configured below, by default it takes of 3/4 of
+  -- the available space. Remaining windows tile to both the left and
+  -- right of the master window. You can resize using "super-h" and
+  -- "super-l".
+  -- ||| ThreeColMid 1 (3/100) (3/4)
+
+  -- Circle layout places the master window in the center of the screen.
+  -- Remaining windows appear in a circle around it
+  -- ||| Circle
+
+  -- Grid layout tries to equally distribute windows in the available
+  -- space, increasing the number of columns and rows as necessary.
+  -- Master window is at top left.
+  ||| Grid))
