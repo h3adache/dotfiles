f5baa2dee452854461f16d0e45b6fdad563d72bc
Tim Chen <tchen@clearstreet.io>
2025-03-28 18:40:53 -0400

    refactor(base): revamps and cleanups.

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@
 set -ve
 CHEZMOI_BIN_DIR=$HOME/.local/bin
 
-sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b $CHEZMOI_BIN_DIR
-exec $CHEZMOI_BIN_DIR/chezmoi init h3adache --apply --ssh
+sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b $CHEZMOI_BIN_DIR init h3adache --apply
